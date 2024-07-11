3c4d26837f5106aa1aac1cf5753775405e9302cd
Henrique Godoy <131921592+Henrique-G15@users.noreply.github.com>
2024-07-11 14:05:23 -0300

    Update ConexaoDB.java
    alternado a senha

--- src/main/java/sl/ConexaoDB.java
+++ src/main/java/sl/ConexaoDB.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConexaoDB {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/tdelino?useSSL=false";
     private static final String USER = "root";
-    private static final String PASSWORD = "godoy12305godoy";
+    private static final String PASSWORD = "senha";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
